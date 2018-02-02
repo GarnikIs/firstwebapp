2d3eba44d4613d47869c0900b087ea8bbfec3367
GarnikIs <garnikisajan@gmail.com>
2018-02-03 00:15:27 +0400

    ProductType entity is created with id and name

---
+++ core/src/main/java/gar/iso/core/dto/ProductType.java
@@ -0,0 +1,42 @@
+package gar.iso.core.dto;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class ProductType implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_type_id")
+    private int productTypeId;
+
+    @Column(name = "product_type_name")
+    private String productTypeName;
+
+    public int getProductTypeId() {
+        return productTypeId;
+    }
+
+    public void setProductTypeId(int productTypeId) {
+        this.productTypeId = productTypeId;
+    }
+
+    public String getProductTypeName() {
+        return productTypeName;
+    }
+
+    public void setProductTypeName(String productTypeName) {
+        this.productTypeName = productTypeName;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductType{" +
+                "productTypeId=" + productTypeId +
+                ", productTypeName='" + productTypeName +
+                '}';
+    }
+}
